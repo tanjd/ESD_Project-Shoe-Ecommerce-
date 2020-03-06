75f65edc73400aef5c3e07820cf6bab89221dc6d
Veronica <xuanhui.lee.2018@sis.smu.edu.sg>
2020-03-06 14:00:59 +0800

    updated methods into product.py
    Co-Authored-By: miketyj <miketyj@users.noreply.github.com>

--- product/product.py
+++ product/product.py
@@ -56,11 +56,51 @@ class Category(db.Model):
         return {"id": self.id, "name": self.name}
 
 
-@app.route('/')
-def home():
-    # return config.myname
-    # return 'Hello, Flask!'
-    return jsonify({"product": [Product.json() for Product in Product.query.all()]})
+
+
+# [GET] all products
+@app.route("/product")
+def get_all(): 
+    return jsonify({"product": [Product.json() for product in Product.query.all()]})
+
+# [GET] all categories
+@app.route("/category")
+def get_all(): 
+    return jsonify({"category": [Category.json() for category in Category.query.all()]})
+
+# [GET] products by category
+@app.route("/product/<string:category_id>")
+def find_by_category_id (category_id): 
+    product = Product.query.filter_by (category_id = category_id)
+    if product: 
+        return jsonify(product.json())
+    return jsonify({"message": "Products not found"}), 404
+
+# [GET] products by name **NEED TO LOOK INTO THIS
+@app.route("/product/<string:name>")
+def find_by_name (name): 
+    product = Product.query.filter_by (name = name)
+    if product: 
+        return jsonify(product.json())
+    return jsonify({"message": "Products not found"}), 404
+
+# [POST] create product
+@app.route("/book/<string:id>", methods = ['POST'])
+def create_product(id): 
+    if (Product.query.filter_by (id = id).first()): 
+        return jsonify({"message": "A product with id '{}' already exists.".format(id)}), 400 
+
+        data = request.get.json()
+        product = Product(id, **data) # get all fields (**)
+
+        try: 
+            db.session.add(product)
+            db.session.commit()
+
+        except: 
+            return jsonify({"message": "An error occurred creating the product"}), 500
+
+        return jsonify(product.json()), 201
 
 
 if __name__ == "__main__":
