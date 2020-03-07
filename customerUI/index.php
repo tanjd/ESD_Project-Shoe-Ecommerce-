c5be1ec9b75cdfb37fc5d4add7a2819587849ebc
Jeddy's Desktop <42729752+tanjd@users.noreply.github.com>
2020-03-07 20:07:26 +0800

    template for website

---
+++ customerUI/index.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php
+require_once 'template/head.php';
+?>
+
+<body>
+    <?php
+    require_once 'template/header.php';
+    ?>
+
+    <main role="main" class="container">
+        <div class="starter-template">
+            <h1>Bootstrap starter template</h1>
+            <p class="lead">ALL YOUR HTML CODES WILL COME IN HERE</p>
+        </div>
+    </main>
+
+    <?php
+    require_once 'template/footer.php';
+    ?>
+</body>
+
+</html>
---
+++ customerUI/template/head.php
@@ -0,0 +1,29 @@
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+  <title>Starter Template for Bootstrap</title>
+
+  <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/starter-template/">
+
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <!-- Custom styles for this template -->
+  <style>
+    body {
+      padding-top: 5rem;
+    }
+
+    .starter-template {
+      padding: 3rem 1.5rem;
+      text-align: center;
+    }
+
+    .footer {
+      background-color: #f5f5f5;
+    }
+  </style>
+</head>
