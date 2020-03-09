fe7e3190af2ccd77d7f33eb7ccc10fc4ab3e623d
miketyj <54625353+miketyj@users.noreply.github.com>
2020-03-09 13:52:04 +0800

    Added product_url with port 5001

--- customerUI/include/microservice_config.php
+++ customerUI/include/microservice_config.php
@@ -1,7 +1,7 @@
 <?php
 
     $customer_url = 'http://localhost:5000/';
-    // $product_url = 'http://localhost:5000/';
+    $product_url = 'http://localhost:5001/';
     // $order_url = 'http://localhost:5000/';
 
 ?>
