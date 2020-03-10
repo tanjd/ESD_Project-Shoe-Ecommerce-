bf7aaae7a7636cc462c4edb93d9a7b0f21c126c4
Jeddy's Desktop <42729752+tanjd@users.noreply.github.com>
2020-03-10 10:59:42 +0800

    Create set_password.php

---
+++ customerUI/set_password.php
@@ -0,0 +1,46 @@
+<?php
+require_once 'include/autoload.php';
+// if (isset($_SESSION['customer_id'])) {
+
+//     $customer_id = $_SESSION['customer_id'];
+
+//     $POST_data = [
+//         "customer_id" => $customer_id,
+//     ];
+//     $data = CallAPI('GET', $customer_url, 'get_customer/', $POST_data);
+//     if ($data != false) {
+//         $customer = $data->{'customer'};
+//     } else {
+//         $customer = false;
+//     }
+?>
+
+<?php
+require_once 'template/head.php';
+require_once 'template/header.php';
+?>
+<main role="main" class="container">
+    <!-- <div class="starter-template"> -->
+    <p class="lead">Set Password</p>
+    <form>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="" readonly>
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="password" name="password">
+        </div>
+        <div class="form-group">
+            <label for="confirm_password">Confirm Password</label>
+            <input type="password" class="form-control" id="confirm_password" name="confirm_password">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    <!-- </div> -->
+
+</main>
+<?php
+require_once 'template/footer.php';
+?>
