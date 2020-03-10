040ae0cfdf498b7ec8aad53abb2a1d986829867f
miketyj <54625353+miketyj@users.noreply.github.com>
2020-03-10 15:36:45 +0800

    Updated product database load file

---
+++ product/product_cat_load.py
@@ -0,0 +1,29 @@
+# Run this to load product database
+import csv
+import mysql.connector
+
+mydb =mysql.connector.connect(host='localhost', user='root', password='', database='product_db')
+print("database connected")
+
+cursor =mydb.cursor()
+csv_data =csv.reader(open('category.csv'))
+for row in csv_data:
+    cursor.execute("INSERT INTO category (id, name) VALUES (%s, %s)", row)
+    print(row)
+
+mydb.commit()
+cursor.close()
+
+mydb =mysql.connector.connect(host='localhost', user='root', password='', database='product_db')
+print("database connected")
+
+cursor =mydb.cursor()
+csv_data =csv.reader(open('product.csv'))
+for row in csv_data:
+    cursor.execute("INSERT INTO product (image, name, category_id, description, unit_price, quantity) VALUES (%s, %s, %s, %s, %s, %s)", row)
+    print(row)
+
+mydb.commit()
+cursor.close()
+
+print('DONE')
--- product/product_load.py
+++ product/product_load.py
@@ -1,15 +1,16 @@
-import csv
-import mysql.connector
+#IGNORE THIS FILE
+# import csv
+# import mysql.connector
 
-mydb =mysql.connector.connect(host='localhost', user='root', password='', database='product_db')
-print("database connected")
+# mydb =mysql.connector.connect(host='localhost', user='root', password='', database='product_db')
+# print("database connected")
 
-cursor =mydb.cursor()
-csv_data =csv.reader(open('product.csv'))
-for row in csv_data:
-    cursor.execute("INSERT INTO product (image, name, category_id, description, unit_price, quantity) VALUES (%s, %s, %s, %s, %s, %s)", row)
-    print(row)
+# cursor =mydb.cursor()
+# csv_data =csv.reader(open('product.csv'))
+# for row in csv_data:
+#     cursor.execute("INSERT INTO product (image, name, category_id, description, unit_price, quantity) VALUES (%s, %s, %s, %s, %s, %s)", row)
+#     print(row)
 
-mydb.commit()
-cursor.close()
-print('DONE')
+# mydb.commit()
+# cursor.close()
+# print('DONE')
