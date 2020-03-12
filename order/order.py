a88c809c208a74610b407008e031279b78ed7fe5
kqyeong2018 <54661048+kqyeong2018@users.noreply.github.com>
2020-03-12 12:14:16 +0800

    Order.py updated
    -updated all db
    -POST function done (Except auto_increment for ID and invoice_ID)
    -GET function for order by invoice_id
    -GET function for invoice by invoice_id

--- order/order.py
+++ order/order.py
@@ -9,33 +9,8 @@ app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/order_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
 CORS(app)
-
-
-class Order(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    customer_id = db.Column(db.Integer, nullable=False)
-    product_id = db.Column(db.String(120), nullable=False, primary_key=True)
-    quantity = db.Column(db.Integer, nullable=False)
-    price = db.Column(db.Float(6,2),nullable=False)
-    timestamp = db.Column(
-        db.DateTime, server_default=func.now(), nullable=False)
-
-
-    def __init__(self, id, customer_id, product_id, quantity, price, timestamp):
-        self.id = id
-        self.customer_id = customer_id
-        self.product_id = product_id
-        self.quantity = quantity
-        self.price = price
-        self.timestamp = timestamp
-
-    def json(self):
-        return {"id": self.id, "customer_id": self.customer_id, "product_id": self.product_id, "quantity": self.quantity, "price": self.price,
-                "timestamp": self.timestamp}
-
 
 class Order_invoice(db.Model):
     id = db.Column(db.Integer,primary_key=True)
@@ -44,24 +19,103 @@ class Order_invoice(db.Model):
 
     Orders = db.relationship('Order',backref='orders',lazy=True)
 
-    def __init__(self, id, total_amount, customer_id):
+    def init(self, id, total_amount, customer_id):
         self.id = id
         self.customer_id = customer_id
         self.total_amount = total_amount
+    
+    def json(self):
+         return {"id": self.id, "customer_id": self.customer_id, "total_amount": self.total_amount}
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    invoice_id = db.Column(db.Integer, db.ForeignKey('order_invoice.id'), nullable = False , primary_key = True)
+    customer_id = db.Column(db.Integer, nullable=False)
+    product_id = db.Column(db.String(120), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float(6,2),nullable=False)
+    timestamp = db.Column(
+        db.DateTime, server_default=func.now(), nullable=False)
+
+    def init(self, id, invoice_id, customer_id, product_id, quantity, price, timestamp):
+        self.id = id
+        self.invoice_id = invoice_id
+        self.customer_id = customer_id
+        self.product_id = product_id
+        self.quantity = quantity
+        self.price = price
+        self.timestamp = timestamp
 
     def json(self):
-        return {"id": self.id, "customer_id": self.customer_id, "total_amount": self.total_amount}
+        return {"id": self.id, "invoice_id": self.invoice_id, "customer_id": self.customer_id, "product_id": self.product_id, "quantity": self.quantity, "price": self.price,
+                "timestamp": self.timestamp}
 
+    def json(self):
+        return {"id": self.id, "customer_id": self.customer_id, "product_id": self.product_id, "quantity": self.quantity, "price": self.price,
+                "timestamp": self.timestamp}
 
+@app.route('/create_order', methods=['POST'])
+def create_order():
+    order_data = request.get_json()
+    
+    cart = order_data['cart']
+    customer_id = order_data['id']
 
+    total = 0
+    for c_list in cart:
+        product_price = c_list['unit_price']
+        total += product_price
+        #print(product_price)
 
+    new_order_invoice = Order_invoice( customer_id = order_data['id'], total_amount = total, 
+    id = 555) #find sth to generate id)
 
-@app.route('/')
-def home():
-    # return config.myname
-    return 'Hello, Flask!'
-    #return jsonify({"customer": [Customer.json() for Customer in Customer.query.all()]})
+    try:
+        db.session.add(new_order_invoice)
+        db.session.commit()
+    
+    except:
+            return jsonify({"status": "fail",
+                        "message": "An error occurred creating order invoice."})
+    #return jsonify({"status": "success"})
+    
+    for c_list in cart:
+        try:
+            product_price = c_list['unit_price']
+            product_id = c_list['id']
+            quantity = c_list['quantity']
+            new_order = (Order( id= 333, invoice_id = 555,
+                                    customer_id = customer_id,
+                                    product_id = product_id,
+                                    quantity = quantity,
+                                    price = product_price))
+            db.session.add(new_order)
+            db.session.commit()
+    
+        except:
+            return jsonify({"status": "fail",
+                        "message": "An error occurred creating order."})
+        return jsonify({"status": "success"})
 
+@app.route('/get_invoice/<string:id>')
+def get_invoice(id):
+    invoice = Order_invoice.query.filter_by(id=id).first()
+    if invoice:
+        return_message = ({"status": "success",
+                           "invoice": invoice.json()})
+    else:
+        return_message = ({"status": "fail"})
+    return jsonify(return_message)
+
+@app.route("/get_all_orders/<string:invoice_id>")
+def get_all_orders(invoice_id):
+    order = [order.json() for order in Order.query.filter_by(invoice_id=invoice_id).all()]
+    if order:
+        return_message =({"status": "success",
+                          "order": order})
+    else:
+        return_message = ({"status": "fail"})
+    return jsonify(return_message)
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5003, debug=True)
