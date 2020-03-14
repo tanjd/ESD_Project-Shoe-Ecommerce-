e6f0223d231565bf780d98335c9461988f3ecda1
shannoncyj <shannonchew.2018@sis.smu.edu.sg>
2020-03-14 20:25:13 +0800

    Update paypal.php

--- customerUI/paypal.php
+++ customerUI/paypal.php
@@ -59,6 +59,7 @@
     },
     onApprove: function(data, actions) {
       // This function captures the funds from the transaction.
+      window.location.href="http://localhost/esd/customerui/try.php";
       return actions.order.capture().then(function(details) {
         // This function shows a transaction success message to your buyer.
         alert('Transaction completed by ' + details.payer.name.given_name);
