2e056feb8b0e143798886bc7a2dfd5ae4985f3d5
shannoncyj <shannonchew.2018@sis.smu.edu.sg>
2020-03-15 02:28:31 +0800

    created delivery microservice

--- customerUI/include/microservice_config.php
+++ customerUI/include/microservice_config.php
@@ -2,7 +2,7 @@
 
     $customer_url = 'http://localhost:5000/';
     $product_url = 'http://localhost:5001/';
-    
+    $delivery_url = 'http://localhost:5002/';
     $order_url = 'http://localhost:5003/';
 
 ?>
