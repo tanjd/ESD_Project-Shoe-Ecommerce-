9afa2c64d244c0f40044de743889ffdd180efe89
Jeddy's Desktop <42729752+tanjd@users.noreply.github.com>
2020-03-21 14:36:54 +0800

    updated customer_data.py

--- customer/customer_data.py
+++ customer/customer_data.py
@@ -1,28 +1,52 @@
 customers = [
     {
         'email': 'tanjd@hotmail.com',
-        'name': 'jeddy',
-        'telegram_id': '@jeddytan',
+        'name': 'jeddy tan',
+        'telegram_id': '99770482',
         'password': 'hello',
-        'address': '10a bendemeer road',
-        'postal_code': '331010'
+        'address': '123 smu road',
+        'postal_code': '123456'
     },
     {
-        'email': 'firejd@hotmail.com',
-        'name': 'jeddy1',
-        'telegram_id': '@jeddytan',
+        'email': '',
+        'name': 'mike teng',
+        'telegram_id': '167560833',
         'password': 'hello',
-        'address': '10a bendemeer road',
-        'postal_code': '331010'
+        'address': '123 smu road',
+        'postal_code': '123456'
     },
     {
-        'email': 'waterjd@hotmail.com',
-        'name': 'jeddy2',
-        'telegram_id': '@jeddytan',
+        'email': '',
+        'name': 'lee xuan hui',
+        'telegram_id': '',
         'password': 'hello',
-        'address': '10a bendemeer road',
-        'postal_code': '331010'
+        'address': '123 smu road',
+        'postal_code': '123456'
     },
+    {
+        'email': '',
+        'name': 'bryan chin',
+        'telegram_id': '',
+        'password': 'hello',
+        'address': '123 smu road',
+        'postal_code': '123456'
+    },
+    {
+        'email': '',
+        'name': 'shannon',
+        'telegram_id': '',
+        'password': 'hello',
+        'address': '123 smu road',
+        'postal_code': '123456'
+    },
+    {
+        'email': '',
+        'name': 'ashley',
+        'telegram_id': '',
+        'password': 'hello',
+        'address': '123 smu road',
+        'postal_code': '123456'
+    }
 ]
 
 # INSERT INTO `customer`(`email`, `name`, `telegram_id`, `password`, `address`) VALUES ('tanjd@hotmail.com' ,'jeddy','@jeddytan','hello','331010')
