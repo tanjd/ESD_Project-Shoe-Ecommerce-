ed8f21cc39c1f51794a45416422cb5c016e728b6
Veronica <xuanhui.lee.2018@sis.smu.edu.sg>
2020-03-22 11:55:27 +0800

    updated customer_data

--- customer/customer_data.py
+++ customer/customer_data.py
@@ -46,6 +46,14 @@ customers = [
         'password': 'hello',
         'address': '123 smu road',
         'postal_code': '123456'
+    },
+    {
+        'email': 'veronicalee.swift23@gmail.com', 
+        'name': 'Veronica', 
+        'telegram_id': '360279534', 
+        'password': 'hello',
+        'address': '123 smu road',
+        'postal_code': '123456'
     }
 ]
 
