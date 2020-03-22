25263be75dd2650770d4b5594f5b82dd49309f1a
tanjd <42729752+tanjd@users.noreply.github.com>
2020-03-22 12:26:14 +0800

    update custer_data.py

--- customer/customer_data.py
+++ customer/customer_data.py
@@ -16,17 +16,9 @@ customers = [
         'postal_code': '123456'
     },
     {
-        'email': '',
-        'name': 'lee xuan hui',
-        'telegram_id': '',
-        'password': 'hello',
-        'address': '123 smu road',
-        'postal_code': '123456'
-    },
-    {
-        'email': '',
+        'email': 'junkit_chin@hotmail.com',
         'name': 'bryan chin',
-        'telegram_id': '',
+        'telegram_id': '287424423',
         'password': 'hello',
         'address': '123 smu road',
         'postal_code': '123456'
@@ -48,9 +40,9 @@ customers = [
         'postal_code': '123456'
     },
     {
-        'email': 'veronicalee.swift23@gmail.com', 
-        'name': 'Veronica', 
-        'telegram_id': '360279534', 
+        'email': 'veronicalee.swift23@gmail.com',
+        'name': 'Veronica',
+        'telegram_id': '360279534',
         'password': 'hello',
         'address': '123 smu road',
         'postal_code': '123456'
