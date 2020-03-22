61308ea70e6fdd505e0b4e6e7171046cb372b44a
bryanchin <bryan.chin.2018@smu.edu.sg>
2020-03-22 12:23:41 +0800

    remove var_dump

--- customerUI/delete.php
+++ customerUI/delete.php
@@ -8,9 +8,7 @@ if (isset($_GET["message_id"])) {
     ];
     
     $message_data = CallAPI('GET', $message_url, 'delete_message/', $GET_data);
-    var_dump($message_data);
     $message_status = checkSuccessOrFailure($message_data);
-    var_dump($message_status);
     if ($message_status != false) {
         
         header('Location: read_msg.php');
