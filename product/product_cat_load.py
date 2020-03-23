86652f27b84225aa96c12fdf046dcc0bf1352ae5
tanjd <42729752+tanjd@users.noreply.github.com>
2020-03-24 02:00:09 +0800

    updated product db

--- product/product_cat_load.py
+++ product/product_cat_load.py
@@ -1,26 +1,29 @@
 # Run this to load product database
 import csv
 import mysql.connector
+import sqlite3
+mydb = sqlite3.connect('product_db.db')
 
-mydb =mysql.connector.connect(host='localhost', user='root', password='', database='product_db')
+# mydb =mysql.connector.connect(host='localhost', user='root', password='', database='product_db')
 print("database connected")
 
 cursor =mydb.cursor()
 csv_data =csv.reader(open('category.csv'))
 for row in csv_data:
-    cursor.execute("INSERT INTO category (id, name) VALUES (%s, %s)", row)
+    cursor.execute("INSERT INTO category (id, name) VALUES (?, ?)", row)
     print(row)
 
-mydb.commit()
-cursor.close()
+# mydb.commit()
+# cursor.close()
 
-mydb =mysql.connector.connect(host='localhost', user='root', password='', database='product_db')
-print("database connected")
+# mydb =mysql.connector.connect(host='localhost', user='root', password='', database='product_db')
+# print("database connected")
 
-cursor =mydb.cursor()
+# cursor =mydb.cursor()
+
 csv_data =csv.reader(open('product.csv'))
 for row in csv_data:
-    cursor.execute("INSERT INTO product (image, name, category_id, description, unit_price, quantity) VALUES (%s, %s, %s, %s, %s, %s)", row)
+    cursor.execute("INSERT INTO product (image, name, category_id, description, unit_price, quantity) VALUES (?, ?, ?, ?, ?, ?)", row)
     print(row)
 
 mydb.commit()
