78dfe5f29e4dae18ae366e917d728235aac6aa78
bryanchin <bryan.chin.2018@smu.edu.sg>
2020-03-25 13:20:11 +0800

    admin index

--- adminUI/index.php
+++ adminUI/index.php
@@ -7,4 +7,32 @@ require_once 'include/autoload.php';
 <link rel="stylesheet" href="css/login_style.css">
 <link rel="stylesheet" href="css/use.fontawesome.comv5.3.1cssall" \ integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
-<a href="send_message.php" class="btn btn-danger btn-lg active btn-block" role="button" aria-pressed="true" >Broadcast Message</a>
+<main role="main" class="container">
+<div class="container-fluid">
+<h1 class="mt-4">Dashboard</h1>
+<ol class="breadcrumb mb-4">
+<li class="breadcrumb-item active">Dashboard</li>
+</ol>
+<div class="row">
+<div class="col-xl-3 col-md-6">
+    <div class="card bg-danger text-white mb-4">
+        <div class="card-body">Broadcast Message</div>
+        <div class="card-footer d-flex align-items-center justify-content-between">
+            <a class="small text-white stretched-link" href="send_message.php">View Details</a>
+            <div class="small text-white"><svg class="svg-inline--fa fa-angle-right fa-w-8" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512" data-fa-i2svg=""><path fill="currentColor" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg><!-- <i class="fas fa-angle-right"></i> --></div>
+        </div>
+    </div>
+</div>
+<div class="col-xl-3 col-md-6">
+    <div class="card bg-warning text-white mb-4">
+        <div class="card-body">View Delivery</div>
+        <div class="card-footer d-flex align-items-center justify-content-between">
+            <a class="small text-white stretched-link" href="view_delivery.php">View Details</a>
+            <div class="small text-white"><svg class="svg-inline--fa fa-angle-right fa-w-8" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512" data-fa-i2svg=""><path fill="currentColor" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg><!-- <i class="fas fa-angle-right"></i> --></div>
+        </div>
+    </div>
+</div>
+
+</div>
+</main>
+                       
--- adminUI/send_message.php
+++ adminUI/send_message.php
@@ -10,15 +10,10 @@ $categories_data = CALLAPI('GET', $product_url, 'get_all_categories');
 
 <!doctype html>
   <html lang="en">
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    </main>
-  </head>
   <body>
 
     <div class="container">
