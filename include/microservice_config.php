b3f738a76042276f49924d30f063e7b562c32c88
tanjd <42729752+tanjd@users.noreply.github.com>
2020-03-25 18:04:29 +0800

    fixed the header bug

--- include/microservice_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $customer_url = 'http://localhost:5000/';
-    $product_url = 'http://localhost:5001/';
-    $delivery_url = 'http://localhost:5002/';
-    $order_url = 'http://localhost:5003/';
-
-?>
