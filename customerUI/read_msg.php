1291b442bc1775db34e65d0ce2e9fb5b4de264fa
bryanchin <bryan.chin.2018@smu.edu.sg>
2020-03-25 18:26:58 +0800

    Update read_msg.php

--- customerUI/read_msg.php
+++ customerUI/read_msg.php
@@ -72,13 +72,13 @@ if($is_loggedin){
                     }
                 }
                 else{
-                    echo "Please login to view messages in inbox";
+                    echo "<p></p>Please login to view messages in inbox";
                 }
                 
                 
                 ?>
                
-                
+
                
             </tbody>
             </table>
@@ -90,6 +90,34 @@ if($is_loggedin){
     </div>
 
 </main>
+<main role="main" class="container" id="d">
+<div class="my-3 p-3 bg-white rounded shadow-sm">
+    <h6 class="border-bottom border-gray pb-2 mb-0">Recent updates</h6>
+    <?php
+     $count=0;
+     if($msg_data->status!='fail'){
+     foreach ($message as $msg) {
+         if($msg->status ==0){
+    
+        echo "<div class='media text-muted pt-3'>
+                <svg class='bd-placeholder-img mr-2 rounded' width='32' height='32' xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMidYMid slice' focusable='false' role='img' aria-label='Placeholder: 32x32'><title>Placeholder</title><rect width='100%' height='100%' fill='#007bff'></rect><text x='50%' y='50%' fill='#007bff' dy='.3em'>32x32</text></svg>
+            <p class='media-body pb-3 mb-0 small lh-125 border-bottom border-gray'>
+            <strong class='d-block text-gray-dark'>Python shoes</strong>
+            {$msg->content_message}
+            
+        </p>
+        <a href='delete.php?message_id={$msg->id}&from=read_msg.php'> <i id='testing' class='fas fa-trash' style='color:red; padding-right: 30px;'></i></a>
+        <a href='seen.php?message_id={$msg->id}&from=read_msg.php'> <i id='testing' class='far fa-eye' style='font-size: 20px; color:#4ee44e; padding-right: 30px;'></i></a>
+        </div>";
+         }
+        
+        }
+    }
+
+    ?>
+        
+</main>
+  
 
 <?php
 
