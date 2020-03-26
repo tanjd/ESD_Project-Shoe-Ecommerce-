da4f90c65a2a9ba669591b1506abaac7a29b81e6
bryanchin <bryan.chin.2018@smu.edu.sg>
2020-03-27 04:25:25 +0800

    Update send_message.php

--- adminUI/send_message.php
+++ adminUI/send_message.php
@@ -12,7 +12,10 @@ $categories = $categories_data->categories
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
 
@@ -57,7 +60,35 @@ $categories = $categories_data->categories
           <input type="text" class="form-control" name="message2" id="message" placeholder="Enter Message">
         </div>
         <p>
-        <button type="submit" name='submit2' class="btn btn-success btn-block"><i class="fas fa-paper-plane"></i> Broadcast Message</button>
+        <button type="button" class="btn btn-success btn-block" data-toggle="modal" data-target="#myModal">
+        <i class="fas fa-paper-plane"></i> Broadcast Message
+        </button>
+        <div class="modal fade" id="myModal">
+        <div class="modal-dialog">
+          <div class="modal-content">
+          
+            <!-- Modal Header -->
+            <div class="modal-header">
+              <h4 class="modal-title">Sending Message...</h4>
+              <button type="button" class="close" data-dismiss="modal">×</button>
+            </div>
+            
+            <!-- Modal body -->
+            <div class="modal-body">
+              Sending this message to all relevant customers. <br>
+              Are you sure you want to continue? <br>
+              Press 'OK' to continue, or Cancel to stay on current page.
+            </div>
+            
+            <!-- Modal footer -->
+            <div class="modal-footer">
+              <button type="submit" name='submit2' class="btn btn-success ">OK</button>
+              <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+            </div>
+            
+          </div>
+        </div>
+      </div>
       </form></p> 
   </div>
   
