74a29e546295fd3e9cadb45e2d2228659e01518d
bryanchin <bryan.chin.2018@smu.edu.sg>
2020-03-27 03:52:00 +0800

    admin index.php

--- adminUI/index2.php
+++ adminUI/index2.php
@@ -4,44 +4,145 @@ require_once 'template/header.php';
 require_once 'include/autoload.php';
 ?>
 
-
-
-
-
-<link rel="stylesheet" href="css/login_style.css">
-<link rel="stylesheet" href="css/use.fontawesome.comv5.3.1cssall" \ integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-<?php
-if (isset($_SESSION['Admin']) && $_SESSION['Admin'] == 'Admin'){ ?>
-
+<!DOCTYPE html>
+<html lang="en">
+<style>
+.text-small {
+  font-size: 0.75rem;
+}
+</style>
+<body>
 <main role="main" class="container">
-<div class="container-fluid">
-<h1 class="mt-4">Dashboard</h1>
-<ol class="breadcrumb mb-4">
-<li class="breadcrumb-item active">Dashboard</li>
-</ol>
+    <!-- Grid row -->
 <div class="row">
-<div class="col-xl-3 col-md-6">
-    <div class="card bg-danger text-white mb-4">
-        <div class="card-body">Broadcast Message</div>
-        <div class="card-footer d-flex align-items-center justify-content-between">
-            <a class="small text-white stretched-link" href="send_message.php">View Details</a>
-            <div class="small text-white"><svg class="svg-inline--fa fa-angle-right fa-w-8" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512" data-fa-i2svg=""><path fill="currentColor" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg><!-- <i class="fas fa-angle-right"></i> --></div>
-        </div>
+
+<!-- Grid column -->
+<div class="col-lg-4 col-md-12">
+
+  <!--Card-->
+  <div class="card">
+
+    <!--Card image-->
+    <div class="view">
+      <img src="https://www.serverless360.com/wp-content/uploads/2015/09/Messaging-Capabilities.jpg" class="card-img-top"
+        alt="photo">
+      <a href="#">
+        <div class="mask rgba-white-slight"></div>
+      </a>
     </div>
-</div>
-<div class="col-xl-3 col-md-6">
-    <div class="card bg-warning text-white mb-4">
-        <div class="card-body">View Delivery</div>
-        <div class="card-footer d-flex align-items-center justify-content-between">
-            <a class="small text-white stretched-link" href="view_delivery.php">View Details</a>
-            <div class="small text-white"><svg class="svg-inline--fa fa-angle-right fa-w-8" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512" data-fa-i2svg=""><path fill="currentColor" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg><!-- <i class="fas fa-angle-right"></i> --></div>
-        </div>
+
+    <!--Card content-->
+    <div class="card-body">
+      <!--Title-->
+      <h4 class="card-title">Delivery</h4>
+      <!--Text-->
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+        card's content.</p>
+      <a href="view_delivery.php"><button type="button" class="btn btn-outline-warning btn-rounded waves-effect"><i class="fas fa-truck"></i>
+        </i>View Delivery</button></a>
     </div>
+
+  </div>
+  <!--/.Card-->
+  <br>
+  <div class="card p-3">
+    <blockquote class="blockquote mb-0 card-body">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+      <footer class="blockquote-footer">
+        <small class="text-muted">
+          Someone famous in <cite title="Source Title">Source Title</cite>
+        </small>
+      </footer>
+    </blockquote>
+  </div>
+  <br>
 </div>
+<!-- Grid column -->
+
+<!-- Grid column -->
+<div class="col-lg-4 col-md-6">
+
+  <!--Card-->
+  <div class="card">
+
+    <!--Card image-->
+    <div class="view">
+      <img src="https://braze-marketing-assets.s3.amazonaws.com/images/1400x700-API-Campaigns-Featured.png" class="card-img-top"
+        alt="photo">
+      <a href="#">
+        <div class="mask rgba-white-slight"></div>
+      </a>
+    </div>
+
+    <!--Card content-->
+    <div class="card-body">
+      <!--Title-->
+      <h4 class="card-title">Messaging</h4>
+      <!--Text-->
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+        card's content.</p>
+        <a href="send_message.php"><button type="button" class="btn btn-outline-warning btn-rounded waves-effect"><i class="fas fa-mail-bulk"></i>
+        </i>View Messaging</button></a>
+    </div>
+  </div>
+  <!--/.Card-->
 
 </div>
+<!-- Grid column -->
+<div class="col-lg-4 col-md-6">
+<div class="card">
+
+<!--Card image-->
+<div class="view">
+  <img src="https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2016/04/Computer_Science_2-796x401.jpg" class="card-img-top"
+    alt="photo">
+  <a href="#">
+    <div class="mask rgba-white-slight"></div>
+  </a>
+</div>
+
+<!--Card content-->
+<div class="card-body">
+  <!--Title-->
+  <h4 class="card-title">Orders</h4>
+  <!--Text-->
+  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+    card's content.</p>
+    <a href="view_delivery.php"><button type="button" class="btn btn-outline-warning btn-rounded waves-effect"><i class="fas fa-shopping-cart"></i>
+        </i>View Orders</button></a>
+    </div>
+</div>
+<br>
+<div class="card text-center"">
+        <div class=" card-header success-color white-text">
+        Featured
+        </div>
+        <div class="card-body">
+        <h4 class="card-title">Special title treatment</h4>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a class="btn btn-success btn-sm">Go somewhere</a>
+        <br>
+
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+    </div>
+    <br>
+</div>
+
+
+</div>
+
+<!-- Grid column -->
+<div class="col-lg-4 col-md-6">
+    
+  <!--Panel-->
+    
+</div>
+<!-- Grid column -->
+
+  
+</div>
+ 
 </main>
-<?php }
-?>
-
-                       
+</body>
+</html>
