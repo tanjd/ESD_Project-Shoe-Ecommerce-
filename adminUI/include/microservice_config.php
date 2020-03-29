cb7dfc6eb75d845319cc91a3c86686dffea91b50
miketyj <54625353+miketyj@users.noreply.github.com>
2020-03-29 10:09:22 +0800

    updated adminUI microservice_config.php to include ec2 webservices

--- adminUI/include/microservice_config.php
+++ adminUI/include/microservice_config.php
@@ -6,4 +6,10 @@
     $order_url = 'http://localhost:5003/';
     $message_url = 'http://localhost:5004/';
     
+    // $customer_url = 'http://18.140.5.32:5000/';
+    // $product_url = 'http://18.140.5.32:5001/';
+    // $delivery_url = 'http://18.140.5.32:5002/';
+    // $order_url = 'http://18.140.5.32:5003/';
+    // $message_url = 'http://18.140.5.32:5004/';
+
 ?>
